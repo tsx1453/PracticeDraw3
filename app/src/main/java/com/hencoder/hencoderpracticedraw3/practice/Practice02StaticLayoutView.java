7bce5654e6a40486f1a0383144bee17de30cd895
tsx1453 <1453918362@qq.com>
2018-10-19 09:19:44 +0800

    finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice02StaticLayoutView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice02StaticLayoutView.java
@@ -3,14 +3,19 @@ package com.hencoder.hencoderpracticedraw3.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
+@RequiresApi(api = Build.VERSION_CODES.M)
 public class Practice02StaticLayoutView extends View {
     TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
     String text = "Hello\nHenCoder";
+    StaticLayout staticLayout;
 
     public Practice02StaticLayoutView(Context context) {
         super(context);
@@ -26,6 +31,8 @@ public class Practice02StaticLayoutView extends View {
 
     {
         textPaint.setTextSize(60);
+        staticLayout = StaticLayout.Builder.obtain(text, 0, text.length(),
+                textPaint, 600).build();
     }
 
     @Override
@@ -34,6 +41,8 @@ public class Practice02StaticLayoutView extends View {
 
         // 使用 StaticLayout 代替 Canvas.drawText() 来绘制文字，
         // 以绘制出带有换行的文字
-        canvas.drawText(text, 50, 100, textPaint);
+//        canvas.drawText(text, 50, 100, textPaint);
+        canvas.translate(50, 40);
+        staticLayout.draw(canvas);
     }
 }
